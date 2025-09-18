03c6b0a0346e6be06c1967fbc55d01ec11e13169
chedi <dayehchadi56@gmail.com>
2025-09-18 12:03:30 +0100

    new change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
 import { UsageProvider } from "@/contexts/UsageContext";
 import { ConditionalLayout } from "@/components/conditional-layout";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,10 +39,9 @@ export default function RootLayout({
             forcedTheme="dark"
             disableTransitionOnChange
           >
+            <Analytics />
             <UsageProvider>
-            <ConditionalLayout>
-            {children}
-            </ConditionalLayout>
+              <ConditionalLayout>{children}</ConditionalLayout>
             </UsageProvider>
             <Toaster />
           </ThemeProvider>
